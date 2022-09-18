764261698922bdbe2e913d22d01f227a23ae9011
Dadamuga Mazino <destructicus_lives@yahoo.com>
2022-09-18 18:05:03 +0100

    added routes for admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Admin from "./pages/admin";
 
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="admin" element={<Admin />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter basename="best-boi">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
